3e870598f45c717a0381772967c24fc3a2626c8e
pambokarVT <104223382+pambokarVT@users.noreply.github.com>
2024-04-03 02:40:53 +0530

    Add files via upload

---
+++ get_user_top_tracks.py
@@ -0,0 +1,30 @@
+import os
+import json
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+import spotipy.util as util
+
+cid ="XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX" 
+secret = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+
+os.environ['SPOTIPY_CLIENT_ID']= cid
+os.environ['SPOTIPY_CLIENT_SECRET']= secret
+os.environ['SPOTIPY_REDIRECT_URI']='http://localhost:8888/callback'
+
+username = ""
+client_credentials_manager = SpotifyClientCredentials(client_id=cid, client_secret=secret) 
+sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+scope = 'user-top-read'
+token = util.prompt_for_user_token(username, scope)
+
+if token:
+    sp = spotipy.Spotify(auth=token)
+    results = sp.current_user_top_tracks(limit=50,offset=0,time_range='medium_term')
+    for song in range(50):
+        list = []
+        list.append(results)
+        with open('top50_data.json', 'w', encoding='utf-8') as f:
+            json.dump(list, f, ensure_ascii=False, indent=4)
+else:
+    print("Can't get token for", username)
+
